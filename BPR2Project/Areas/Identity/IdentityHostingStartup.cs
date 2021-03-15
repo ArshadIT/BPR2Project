83c497883b1bad03149141256e4303ed997d8f42
ArshadIT <arshadecon@gmailc.om>
2021-03-15 18:42:35 +0100

    login

---
+++ BPR2Project/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BPR2Project.Areas.Identity.Data;
+using BPR2Project.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BPR2Project.Areas.Identity.IdentityHostingStartup))]
+namespace BPR2Project.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BPR2Project.Data.DbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BPR2Project.Data.DbContext>();
+            });
+        }
+    }
+}
---
+++ BPR2Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BPR2Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
